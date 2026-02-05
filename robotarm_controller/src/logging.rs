c7ebea7a2af3f713e4ded0bf908f6e63f83cbbab
RussHewgill <RussHewgill@users.noreply.github.com>
2026-02-05 12:39:07 -0800

    initial commit

---
+++ robotarm_controller/src/logging.rs
@@ -0,0 +1,45 @@
+use tracing_log::LogTracer;
+use tracing_subscriber::{EnvFilter, prelude::*, registry::Registry};
+
+use anyhow::{Context, Result, anyhow, bail, ensure};
+use tracing::{debug, error, info, trace, warn};
+
+pub fn init_logs() {
+    let _ = std::fs::rename("output.log", "output_prev.log");
+    let _ = std::fs::remove_file("output.log");
+
+    let trace_file =
+        tracing_appender::rolling::never(".", "output.log").with_max_level(tracing::Level::TRACE);
+
+    // LogTracer::init().unwrap();
+
+    let file_layer = tracing_subscriber::fmt::Layer::new()
+        .with_writer(trace_file)
+        .with_file(true)
+        .with_ansi(false)
+        .with_line_number(true)
+        .with_target(true)
+        .with_level(true)
+        .compact()
+        .with_filter(tracing_subscriber::filter::EnvFilter::new(
+            "info,robotarm=trace,eframe=warn,wgpu=warn",
+        ));
+
+    let stderr_layer = tracing_subscriber::fmt::layer()
+        .with_writer(std::io::stderr)
+        .without_time()
+        .with_file(true)
+        .with_line_number(true)
+        .with_target(false)
+        .compact()
+        .with_filter(tracing_subscriber::filter::EnvFilter::new(
+            // "info,robotarm=debug,eframe=warn,wgpu=warn",
+            "warn,robotarm=debug,eframe=warn,wgpu=warn",
+        ));
+
+    let subscriber = tracing_subscriber::registry()
+        .with(file_layer)
+        .with(stderr_layer)
+        .try_init()
+        .unwrap();
+}
---
+++ robotarm_controller/src/main.rs
@@ -0,0 +1,13 @@
+// #![allow(unused_variables)]
+// #![allow(unused_imports)]
+// #![allow(unused_mut)]
+// #![allow(dead_code)]
+// #![allow(unused_doc_comments)]
+// #![allow(unexpected_cfgs)]
+
+// mod logging;
+mod simplefoc;
+
+fn main() {
+    // println!("Hello, world!");
+}
